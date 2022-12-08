135a5f80f91650f552c464de47f57b02ea2c3773
aliaydogdu105 <aliaydogdu105@gmail.com>
2022-12-09 02:25:55 +0300

    aside buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Aside from "./components/Aside";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Aside />
     </div>
   );
 }
---
+++ src/components/Aside.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Aside = () => {
+  return (
+    <aside className="w-48 flex flex-col h-screen" aria-label="Sidebar">
+      {/* "flex-grow "look after made footer */}
+      <div className="overflow-y-auto flex justify-center items-center py-4  bg-white">
+        <ul className="space-y-2 flex flex-col items-center">
+          <li>
+            <button className="w-40 h-60 relative inline-flex items-center justify-center px-10 py-4 overflow-hidden font-mono font-medium tracking-tighter text-white bg-blue-600 rounded-3xl group">
+              <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-blue-700 rounded-3xl group-hover:w-40 group-hover:h-60"></span>
+              <span className="absolute inset-0 w-full h-full -mt-1 rounded-3xl opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
+              <span className="relative">Foreign Exchange</span>
+            </button>
+          </li>
+
+          <li>
+            <button className="w-40 h-60 relative inline-flex items-center justify-center px-10 py-4 overflow-hidden font-mono font-medium tracking-tighter text-white bg-green-600 rounded-3xl group">
+              <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-green-700 rounded-3xl group-hover:w-40 group-hover:h-60"></span>
+              <span className="absolute inset-0 w-full h-full -mt-1 rounded-3xl opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
+              <span className="relative">Cryptocurrency</span>
+            </button>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  );
+};
+
+export default Aside;
