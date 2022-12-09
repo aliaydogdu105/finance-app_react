216b2ee5b27327c4e752dd690b89ec1b4a64dfa0
aliaydogdu105 <aliaydogdu105@gmail.com>
2022-12-09 23:59:52 +0300

    foreignExchange

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Aside from "./components/Aside";
 import Navbar from "./components/Navbar";
+import ForeignExchange from "./pages/ForeignExchange";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <ForeignExchange />
       <Aside />
     </div>
   );
